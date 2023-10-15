d1b0cc2cbc80f80d11d3c2f02f0f8cb127b7c017
NAGARASU.P <88180695+Nagarasu0408@users.noreply.github.com>
2023-10-15 12:17:31 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,11 @@ class Z1pass extends StatelessWidget {
           padding: const EdgeInsets.all(10.0),
           child: CardExample(),
         ),
-        bottomNavigationBar: BottomNavigationBarExample(),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(label: "Home",icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "Issues",icon: Icon(Icons.account_tree_outlined)),
+          BottomNavigationBarItem(label: "Settings",icon: Icon(Icons.settings)),
+        ]),
       ),
     );
 
@@ -40,78 +44,6 @@ class CardExample extends StatelessWidget {
           debugPrint('KCE');
         },
         child:  Image(image: NetworkImage("https://kce.ac.in/new/wp-content/uploads/2022/12/logo-grade-kce-2.png"),),
-      ),
-    );
-  }
-}
-
-class OutPass extends StatelessWidget {
-  const OutPass({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-
-
-
-
-class BottomNavigationBarExample extends StatefulWidget {
-  const BottomNavigationBarExample({super.key});
-
-  @override
-  State<BottomNavigationBarExample> createState() =>
-      _BottomNavigationBarExampleState();
-}
-
-class _BottomNavigationBarExampleState extends State<BottomNavigationBarExample> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
       ),
     );
   }
